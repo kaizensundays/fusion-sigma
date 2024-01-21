d087521571057089db6103b708a6dfd2ca581f50
KaizenSundays <kaizensundays@localhost>
2024-01-21 11:35:19 -0500

    [ fusion-memmap ] DefaultMemMapQueue

--- fusion-memmap/src/main/java/com/kaizensundays/fusion/memmap/DefaultMemMapQueue.kt
+++ fusion-memmap/src/main/java/com/kaizensundays/fusion/memmap/DefaultMemMapQueue.kt
@@ -1,7 +1,12 @@
 package com.kaizensundays.fusion.memmap
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import reactor.core.publisher.Mono
+import java.io.File
+import java.io.RandomAccessFile
 import java.nio.ByteBuffer
+import java.nio.channels.FileChannel
 import java.time.Duration
 
 /**
@@ -9,7 +14,78 @@ import java.time.Duration
  *
  * @author Sergey Chuykov
  */
-class DefaultMemMapQueue : ReactiveQueue<ByteBuffer> {
+class DefaultMemMapQueue(
+    private val baseDir: String,
+    private val queueName: String,
+    private val maxQueueSize: Int
+) : ReactiveQueue<ByteBuffer> {
+
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    companion object {
+        const val MESSAGE_META_SIZE = 4
+        const val QUEUE_META_SIZE = 8
+
+        const val HEAD_BYTE_LOC = 0
+        const val TAIL_BYTE_LOC = 4
+    }
+
+    init {
+        File(baseDir).mkdirs()
+    }
+
+    private val mode = "rw"
+
+    private val fileName = baseDir + File.pathSeparatorChar + queueName
+
+    private val dataFile = RandomAccessFile("$fileName.data", mode)
+    private val metaFile = RandomAccessFile("$fileName.meta", mode)
+
+    private val dataBuffer = dataFile.channel.map(FileChannel.MapMode.READ_WRITE, 0, maxQueueSize.toLong())
+    private val metaBuffer = metaFile.channel.map(FileChannel.MapMode.READ_WRITE, 0, QUEUE_META_SIZE.toLong())
+
+    private fun head(): Int {
+        return metaBuffer.getInt(HEAD_BYTE_LOC)
+    }
+
+    private fun head(loc: Int) {
+        metaBuffer.putInt(HEAD_BYTE_LOC, loc)
+    }
+
+    private fun tail(): Int {
+        return metaBuffer.getInt(TAIL_BYTE_LOC)
+    }
+
+    private fun tail(loc: Int) {
+        metaBuffer.putInt(TAIL_BYTE_LOC, loc)
+    }
+
+    private fun isEmpty(): Boolean = head() == tail()
+
+    private fun isFull(messageSize: Int): Boolean {
+        val head = head()
+        val tail = tail()
+        val totalMessageSize = messageSize + MESSAGE_META_SIZE
+        val dataSize = when {
+            (head < tail) -> {
+                tail - head
+            }
+
+            (head > tail) -> {
+                tail + (maxQueueSize - head)
+            }
+
+            else -> {
+                0
+            }
+        }
+        val freeSpace = maxQueueSize - dataSize
+        return if ((tail + totalMessageSize) % maxQueueSize == head) {
+            true
+        } else {
+            totalMessageSize > freeSpace
+        }
+    }
 
     override fun offer(obj: ByteBuffer, timeout: Duration): Mono<Boolean> {
         return Mono.just(false)
